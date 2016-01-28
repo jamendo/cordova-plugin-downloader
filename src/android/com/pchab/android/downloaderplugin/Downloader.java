e3e3465679c657cd3f569d88fecc0009552e2990
pchab <pierre@chabardes.net>
2016-01-28 16:16:22 +0100

    second commit

--- src/android/com/pchab/android/downloaderplugin/Downloader.java
+++ src/android/com/pchab/android/downloaderplugin/Downloader.java
@@ -1,261 +1,128 @@
-package com.jamendo.cache;
+package com.pchab.android.downloaderplugin;
 
 import android.app.DownloadManager;
-import android.app.DownloadManager.Request;
 import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.database.Cursor;
+import android.preference.PreferenceManager;
 import android.net.Uri;
-import android.os.Environment;
-import android.support.v4.util.LongSparseArray;
 import android.util.Log;
 
-import com.jamendo.JamendoContext;
-import com.jamendo.data.Track;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONObject;
+import org.json.JSONArray;
+import java.util.HashMap;
 
-import java.io.File;
+public class Downloader extends CordovaPlugin {
 
-/**
- * Utility class for downloading files. Once the requested file is downloaded
- * the callback, specified at request time, is invoked.
- */
-public class FileDownloader {
+    public static final String ACTION_DOWNLOAD = "download";
 
-    /**
-     * Extension for downloaded files
-     */
-    private final static String TRACK_FILE_EXT = ".mp3";
+    private static final String TAG = "DownloaderPlugin";
 
-    /**
-     * Local folder in {@see Environment.DIRECTORY_MUSIC} for Jamendo downloaded tracks.
-     */
-    private final static String JAMENDO_FOLDER = "Jamendo";
+    private DownloadManager downloadManager;
+    private HashMap<long, Download> downloadMap;
 
-    /**
-     * For logging purposes.
-     */
-    private final static String TAG = "FileDownloader";
+    @Override
+    protected void pluginInitialize()
+    {
+        Log.d(TAG, "PluginInitialize");
 
-    /**
-     * To be used only if given track download URL isn't set
-     */
-    private final static String BASE_DOWNLOAD_URL = "https://storage-new.newjamendo.com/download/track/%s/mp32/";
-    
-    /**
-     * Application's context.
-     */
-    private JamendoContext mJamendoContext;
-    
-    /**
-     * Download service manager.
-     */
-    private DownloadManager mDownloadManager;
-    
-    /**
-     * Maps the requests ids onto the download details descripting structure.
-     */
-    private LongSparseArray<TrackDownloadInfo> downloadRequests =
-            new LongSparseArray<TrackDownloadInfo>();
+        this.downloadManager = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+        this.downloadMap = new HashMap<>();
 
-    private DownloadedTracksDataSource mDownloadedTracksDataSource;
-    private Uri destinationFolderUri;
-
-    /**
-     * Store an instance of system DownloadManager.
-     * Create local destination folder if needed. Fallback to tmp folder if can't create defined folder.
-     *
-     * @param context
-     *            application's context
-     * @param downloadManager
-     *            system download service manager
-     */
-    public FileDownloader(JamendoContext context, DownloadManager downloadManager) {
-        mJamendoContext = context;
-        mDownloadManager = downloadManager;
-        mDownloadedTracksDataSource = new DownloadedTracksDataSource(context);
-
-        // Set, and create if needed, local destination folder
-        File destinationFolder = new File(Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_MUSIC).getAbsolutePath() + File.separator + JAMENDO_FOLDER);
-        if (destinationFolder.mkdirs())
-            // Clear downloaded tracks table
-            mDownloadedTracksDataSource.removeAll();
-
-        // Fallback directory
-        if (!destinationFolder.isDirectory()) {
-            Log.e(TAG, "Can't create Jamendo directory in " + destinationFolder.toString() +
-                    ". Using folder in app reserved memory");
-            destinationFolder = mJamendoContext.getDir(JAMENDO_FOLDER,
-                    Context.MODE_WORLD_WRITEABLE);
-        }
-        this.destinationFolderUri = Uri.fromFile(destinationFolder);
-
-        // Register receiver for download completion and notification click
-        mJamendoContext.registerReceiver(trackDownloadReceiver,
-                new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
+        IntentFilter intentFilter = new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE);
+        registerReceiver(downloadReceiver, intentFilter);
     }
 
-    /**
-     * Helper method to download a Track, storing it in local downloaded tracks db.
-     *
-     * @return ID of the download request
-     */
-    public long downloadTrack(Track track, DownloadCompletionCallback callback) {
-        if (mDownloadManager != null) {
-            Log.d(TAG, "Downloading track " + track.getId());
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 
-            String artistNameTrackName = track.getArtistName() + " - " + track.getName();
-            Uri.Builder destFileUriBuilder = destinationFolderUri.buildUpon().appendPath(
-                    artistNameTrackName + TRACK_FILE_EXT);
+        Log.d(TAG, "CordovaPlugin: execute " + action);
 
-            // Prepare DownloadManager request
-            DownloadManager.Request request;
-            if (track.getAudioDownload() == null)
-                request = new Request(Uri.parse(String.format(BASE_DOWNLOAD_URL, track.getId())));
-            else
-                request = new Request(Uri.parse(track.getAudioDownload()));
-            request.setDestinationUri(destFileUriBuilder.build());
-            request.allowScanningByMediaScanner();
-            request.setMimeType("audio/mpeg");
-            // Download is visible and shows in the notifications while in progress and after completion.
-            request.setNotificationVisibility(Request.VISIBILITY_VISIBLE);
-            request.setTitle(artistNameTrackName);
+        try {
 
-            // Enqueue download
-            long downloadId = mDownloadManager.enqueue(request);
+            if (ACTION_DOWNLOAD.equals(action)) {
 
-            // Store custom DownloadInfo in requests list
-            TrackDownloadInfo downloadInfo = new TrackDownloadInfo(track, callback,
-                    destFileUriBuilder.build().getPath(), artistNameTrackName);
-            downloadRequests.append(downloadId, downloadInfo);
+                Log.d(TAG, "CordovaPlugin: load " + action);
+                this.download(args, callbackContext);
 
-            return downloadId;
-        } else {
-            throw new IllegalStateException("Download manager not set");
+            }
+
+            return true;
+
+        } catch (Exception e) {
+
+            System.err.println("Exception: " + e.getMessage());
+            callbackContext.error(e.getMessage());
+
+            return false;
         }
+    }    
+
+    private void download(JSONArray args, CallbackContext callbackContext)
+    {
+        Log.d(TAG, "CordovaPlugin: " + ACTION_DOWNLOAD);
+
+        JSONObject arg_object = args.getJSONObject(0);
+
+        Uri uri = Uri.parse(arg_object.getString("url"));
+        Download mDownload = new Download(arg_object.getString("path"), callbackContext);
+
+        DownloadManager.Request request = new DownloadManager.Request(uri);
+        long downloadId = this.downloadManager.enqueue(request);
+
+        // save the download
+        this.downloadMap.put(downloadId, mDownload);
     }
 
-    /* Private methods */
-
-    /**
-     * Checks whether the download of the given id is completed or not.
-     *
-     * @param id
-     *            download id to check
-     * @return true if download is completed, false otherwise
-     */
-    private boolean isDownloadCompleted(long id) {
-        DownloadManager.Query query = new DownloadManager.Query();
-        query.setFilterById(id);
-        Cursor cursor = mDownloadManager.query(query);
-        if (cursor.moveToFirst()) {
-            int columnIndex = cursor.getColumnIndex(DownloadManager.COLUMN_STATUS);
-            int status = cursor.getInt(columnIndex);
-
-            return status == DownloadManager.STATUS_SUCCESSFUL;
-        }
-
-        return false;
-    }
-
-    /* Interfaces */
-
-    /**
-     * Interface for the finished download notification.
-     */
-    public interface DownloadCompletionCallback {
-        /**
-         * Notifies that the download has been just completed.
-         *
-         * @param description
-         *            download details
-         */
-        public void downloadCompleted(String description);
-
-        /**
-         * Notifies that the download has failed.
-         *
-         * @param description
-         *            download details
-         */
-        public void downloadFailed(String description);
-    }
-
-    /* Private classes */
-
-    /**
-     * Class containing details about the track download.
-     */
-    private class TrackDownloadInfo {
-        private Track track;
-        private DownloadCompletionCallback callback;
-        private String filePath;
-        private String description;
-
-        public TrackDownloadInfo(Track track, DownloadCompletionCallback callback,
-                                 String filePath, String description) {
-            this.track = track;
-            this.callback = callback;
-            this.filePath = filePath;
-            this.description = description;
-        }
-
-        public Track getTrack() {
-            return track;
-        }
-
-        public DownloadCompletionCallback getCallback() {
-            return callback;
-        }
-
-        public String getFilePath() {
-            return filePath;
-        }
-
-        public String getDescription() {
-            return description;
-        }
-    }
-
-    /* Private instances */
-
-    /**
-     * Receive broadcast when a track download finishes.
-     * Store an entry for downloaded track in SQLite db.
-     */
-    private BroadcastReceiver trackDownloadReceiver = new BroadcastReceiver() {
+    private BroadcastReceiver downloadReceiver = new BroadcastReceiver() {
+  
         @Override
         public void onReceive(Context context, Intent intent) {
-            long downloadId = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1);
-            TrackDownloadInfo downloadInfo = downloadRequests.get(downloadId);
 
-            if (DownloadManager.ACTION_DOWNLOAD_COMPLETE.equals(intent.getAction())
-                    && downloadId != -1 && downloadInfo != null
-                    && downloadInfo.getCallback() != null) {
-                Track track = downloadInfo.getTrack();
-                DownloadCompletionCallback callback = downloadInfo.getCallback();
+            DownloadManager.Query query = new DownloadManager.Query();
+            long downloadId = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, 0);
+            query.setFilterById(downloadId);
+            Cursor cursor = this.downloadManager.query(query);
 
-                if (isDownloadCompleted(downloadId)) {
-                    // Successful download, store track in local db
-                    mDownloadedTracksDataSource.saveTrack(track,
-                            downloadInfo.getFilePath());
+            if (cursor.moveToFirst()){
 
-                    callback.downloadCompleted(downloadInfo.getDescription());
-                } else {
-                    callback.downloadFailed(downloadInfo.getDescription());
+                //Retrieve the saved download
+                Download currentDownload = this.downloadMap.get(downloadId);
+                this.downloadMap.remove(currentDownload);
+
+                int columnIndex = cursor.getColumnIndex(DownloadManager.COLUMN_STATUS);
+                int status = cursor.getInt(columnIndex);
+                int columnReason = cursor.getColumnIndex(DownloadManager.COLUMN_REASON);
+                int reason = cursor.getInt(columnReason);
+                
+                switch (status) {
+                    case DownloadManager.STATUS_SUCCESSFUL:
+                        currentDownload.callbackContext.success();
+                        break;
+                    case DownloadManager.STATUS_FAILED:
+                        currentDownload.callbackContext.error(reason);
+                        break;
+                    case DownloadManager.STATUS_PAUSED:
+                    case DownloadManager.STATUS_PENDING:
+                    case DownloadManager.STATUS_RUNNING:
+                    default:
+                        break;
                 }
             }
-            /* TODO handle download notification click
-            if (DownloadManager.ACTION_NOTIFICATION_CLICKED.equals(intent.getAction())) {
-                Intent downloadedTracksFragmentIntent = new Intent(mJamendoContext,
-                        MainActivity.class);
-                downloadedTracksFragmentIntent.putExtra(IntentKeys.OPEN_FRAGMENT, Screen.FAVOURITES);
-                downloadedTracksFragmentIntent.putExtra(IntentKeys.OPEN_FRAGMENT_TAB, Tabs.DOWNLOADED);
-                mJamendoContext.startActivity(downloadedTracksFragmentIntent);
-            }
-            */
         }
+
     };
+
+    private class Download {
+        public String path;
+        public CallbackContext callbackContext;
+
+        public Download(String path, CallbackContext callbackContext) {
+            this.path = path;
+            this.callbackContext = callbackContext;
+        }
+    }
+
 }
--- www/Downloader.js
+++ www/Downloader.js
@@ -9,8 +9,8 @@ function checkEvent(event) {// add namespace ?
 }
 
 exports.Downloader = {
-    updateInfos: function (infos, successCallback, errorCallback) {
-        console.group("Downloader::updateInfos");
+    download: function (arguments, successCallback, errorCallback) {
+        console.group("Downloader::download");
         console.log(infos);
         console.groupEnd();
 
@@ -18,78 +18,8 @@ exports.Downloader = {
 			successCallback,
 			errorCallback,
 			'Downloader',
-			'updateInfos',
-			[infos]
+			'download',
+			[arguments]
 		);
-    },
-    removePlayer: function (successCallback, errorCallback) {
-        cordova.exec(
-            successCallback,
-            errorCallback,
-            'Downloader',
-            'removePlayer',
-            []
-        )
-    },
-    on: function (event, callback) {
-
-        var eventList = event.split(' ');
-        for (var i = 0; i < eventList.length; ++i) {
-            if (eventList[i].length === 0) {
-                continue;
-            }
-
-            var currentEvent = eventList[i];
-
-            checkEvent(currentEvent);
-            eventCallbackList[currentEvent].push(callback);
-        }
-    },
-    once: function (event, callback) {
-        var onceCallback = (function () {
-            this.off(event, onceCallback);
-            callback.apply(callback, arguments);
-        }).bind(this);
-
-        this.on(event, onceCallback);
-    },
-    off: function (event, callback) {
-        var eventList = event.split(' ');
-        for (var i = 0; i < eventList.length; ++i) {
-            if (eventList[i].length === 0) {
-                continue;
-            }
-
-            var currentEvent = eventList[i];
-
-            if (!(currentEvent in eventCallbackList)) {
-                return;
-            }
-
-            if (callback) {
-                var indexOf = eventCallbackList[currentEvent].indexOf(callback);
-                if (indexOf > -1) {
-                    eventCallbackList[currentEvent].splice(indexOf, 1);
-                }
-            } else {
-                eventCallbackList[currentEvent].splice(0, eventCallbackList[currentEvent].length);
-            }
-            // check current trigger ?
-        }
-    },
-    trigger: function (event) {
-        if (!(event in eventCallbackList)) {
-            return;
-        }
-
-        var data = Array.prototype.slice.call(arguments, 1);
-
-        var callbackList = eventCallbackList[event];
-        for (var i = 0; i < callbackList.length; ++i) {
-            callbackList[i].apply(callbackList[i], data);
-        }
-    },
-    _setEvent: function (event) {
-        this.trigger(event.type);
     }
 };
