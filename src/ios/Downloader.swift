d1f87c0d4c7510f461d5a955d08776518e872f1b
Chabardes <pierre@chabardes.net>
2016-09-08 16:53:20 +0200

    update to swift 3

--- src/ios/Downloader.swift
+++ src/ios/Downloader.swift
@@ -1,60 +1,56 @@
 import Foundation
 
 @objc(Downloader) class Downloader : CDVPlugin {
-    func download(command: CDVInvokedUrlCommand) {
+    func download(_ command: CDVInvokedUrlCommand) {
         var pluginResult = CDVPluginResult(
             status: CDVCommandStatus_ERROR
         )
+        
         var isError = false
 
         let args = command.arguments[0] as! NSDictionary
-        let url = NSURL(string: args["url"] as! String)
-        let targetFile = args["path"] as !String
+        let url = URL(string: args["url"] as! String)
+        let targetFile = args["path"] as! String
 
-        let documentsUrl =  NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).first as NSURL!
-        let destinationUrl = documentsUrl.URLByAppendingPathComponent(targetFile)
+        let documentsUrl =  FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first as URL!
+        let destinationUrl = documentsUrl?.appendingPathComponent(targetFile)
 
-        if NSFileManager().fileExistsAtPath(destinationUrl.path!) {
-            print("file already exists [\(destinationUrl.path!)]")
+        if FileManager().fileExists(atPath: destinationUrl!.path) {
+            print("file already exists [\(destinationUrl?.path)]")
             do {
-                try NSFileManager().removeItemAtPath(destination.path!)
+                try FileManager().removeItem(atPath: destinationUrl!.path)
             }
             catch let error as NSError {
                 pluginResult = CDVPluginResult(
                     status: CDVCommandStatus_ERROR,
-                    messageAsString: error.localizedDescription
+                    messageAs: error.localizedDescription
                 )
-
-                self.commandDelegate!.sendPluginResult(
+                
+                self.commandDelegate!.send(
                     pluginResult,
                     callbackId: command.callbackId
                 )
-
+                
                 isError = true
             }
         }
-
         if !(isError) {
-            let sessionConfig = NSURLSessionConfiguration.defaultSessionConfiguration()
-            let session = NSURLSession(configuration: sessionConfig, delegate: nil, delegateQueue: nil)
-            let request = NSMutableURLRequest(URL: url!)
-            request.HTTPMethod = "GET"
-            let task = session.dataTaskWithRequest(request, completionHandler: { (data: NSData?, response: NSURLResponse?, error: NSError?) -> Void in
+            let sessionConfig = URLSessionConfiguration.default
+            let session = URLSession(configuration: sessionConfig, delegate: nil, delegateQueue: nil)
+            var request = URLRequest(url: url!)
+            request.httpMethod = "GET"
+            let task = session.dataTask(with: request, completionHandler: { (data: Data?, response: URLResponse?, error: Error?) -> Void in
                 if (error == nil) {
-                    if let response = response as? NSHTTPURLResponse {
-                        println("response=\(response)")
+                    if let response = response as? HTTPURLResponse {
                         if response.statusCode == 200 {
-                            if data!.writeToURL(destinationUrl, atomically: true) {
+                            if (try? data!.write(to: destinationUrl!, options: [.atomic])) != nil {
                                 pluginResult = CDVPluginResult(
                                     status: CDVCommandStatus_OK,
-                                    messageAsDictionnary: [
-                                        "folder": destinationUrl.path!,
-                                        "file": targetFile
-                                    ]
+                                    messageAs: documentsUrl?.path
                                 )
-
-                                self.commandDelegate!.sendPluginResult(
-                                    pluginResult, 
+                                
+                                self.commandDelegate!.send(
+                                    pluginResult,
                                     callbackId: command.callbackId
                                 )
                             }
