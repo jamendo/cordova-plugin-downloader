30de4cf5a5ffa40e9cc900c89c8f8787ba6abfd3
Fabian Lauer <fabian@jamendo.com>
2018-05-25 11:44:14 +0200

    handle all condition branches on iOS

--- src/ios/Downloader.swift
+++ src/ios/Downloader.swift
@@ -5,8 +5,6 @@ import Foundation
         var pluginResult = CDVPluginResult(
             status: CDVCommandStatus_ERROR
         )
-        
-        var isError = false
 
         let args = command.arguments[0] as! NSDictionary
         let url = URL(string: args["url"] as! String)
@@ -16,11 +14,9 @@ import Foundation
         let destinationUrl = documentsUrl?.appendingPathComponent(targetFile)
 
         if FileManager().fileExists(atPath: destinationUrl!.path) {
-            print("file already exists [\(destinationUrl?.path)]")
             do {
                 try FileManager().removeItem(atPath: destinationUrl!.path)
-            }
-            catch let error as NSError {
+            } catch let error as NSError {
                 pluginResult = CDVPluginResult(
                     status: CDVCommandStatus_ERROR,
                     messageAs: error.localizedDescription
@@ -31,34 +27,84 @@ import Foundation
                     callbackId: command.callbackId
                 )
                 
-                isError = true
+                return
             }
         }
-        if !(isError) {
-            let sessionConfig = URLSessionConfiguration.default
-            let session = URLSession(configuration: sessionConfig, delegate: nil, delegateQueue: nil)
-            var request = URLRequest(url: url!)
-            request.httpMethod = "GET"
-            let task = session.dataTask(with: request, completionHandler: { (data: Data?, response: URLResponse?, error: Error?) -> Void in
-                if (error == nil) {
-                    if let response = response as? HTTPURLResponse {
-                        if response.statusCode == 200 {
-                            if (try? data!.write(to: destinationUrl!, options: [.atomic])) != nil {
-                                pluginResult = CDVPluginResult(
-                                    status: CDVCommandStatus_OK,
-                                    messageAs: documentsUrl?.path
-                                )
-                                
-                                self.commandDelegate!.send(
-                                    pluginResult,
-                                    callbackId: command.callbackId
-                                )
-                            }
-                        }
+
+        let sessionConfig = URLSessionConfiguration.default
+        sessionConfig.timeoutIntervalForRequest = 5.0
+        let session = URLSession(configuration: sessionConfig, delegate: nil, delegateQueue: nil)
+        var request = URLRequest(url: url!)
+        request.httpMethod = "GET"
+        let task = session.dataTask(with: request, completionHandler: { (data: Data?, response: URLResponse?, error: Error?) -> Void in
+            if (error != nil) {
+                pluginResult = CDVPluginResult(
+                    status: CDVCommandStatus_ERROR,
+                    messageAs: error?.localizedDescription
+                )
+        
+                self.commandDelegate!.send(
+                    pluginResult,
+                    callbackId: command.callbackId
+                )
+                
+                return
+            }
+            
+            if let response = response as? HTTPURLResponse {
+                if response.statusCode == 200 {
+                    if (try? data!.write(to: destinationUrl!, options: [.atomic])) != nil {
+                        pluginResult = CDVPluginResult(
+                            status: CDVCommandStatus_OK,
+                            messageAs: documentsUrl?.path
+                        )
+                        
+                        self.commandDelegate!.send(
+                            pluginResult,
+                            callbackId: command.callbackId
+                        )
+                        
+                        return
+                    } else {
+                        pluginResult = CDVPluginResult(
+                            status: CDVCommandStatus_ERROR,
+                            messageAs: "unable to write downloaded file to \(destinationUrl?.path)"
+                        )
+                        
+                        self.commandDelegate!.send(
+                            pluginResult,
+                            callbackId: command.callbackId
+                        )
+                        
+                        return
                     }
+                } else {
+                    pluginResult = CDVPluginResult(
+                        status: CDVCommandStatus_ERROR,
+                        messageAs: "got response status code \(response.statusCode) (\(destinationUrl?.path))"
+                    )
+            
+                    self.commandDelegate!.send(
+                        pluginResult,
+                        callbackId: command.callbackId
+                    )
+                    
+                    return
                 }
-            })
-            task.resume()
-        }
+            } else {
+                pluginResult = CDVPluginResult(
+                    status: CDVCommandStatus_ERROR,
+                    messageAs: "unable to get response object"
+                )
+        
+                self.commandDelegate!.send(
+                    pluginResult,
+                    callbackId: command.callbackId
+                )
+                
+                return
+            }
+        })
+        task.resume()
     }
 }
